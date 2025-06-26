c0e303d25ddd6c39c4f5a89105699fe08e01ad5c
luisellan <sellanjose2000@gmail.com>
2025-06-25 22:43:09 -0500

    libreria swal

--- views/html/head.php
+++ views/html/head.php
@@ -21,4 +21,10 @@
 <!-- custom Css-->
 <link href="../../assets/css/custom.min.css" rel="stylesheet" type="text/css" />
 
+<!-- SweetAlert2 CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
+
+<!-- SweetAlert2 JS -->
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
 
